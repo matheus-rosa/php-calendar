b96f49db4b66088785f49cc1e46897decc1c1b31
Matheus Rosa <matheus.alves.rosa@outlook.com>
2020-05-25 20:18:22 -0300

    Library core and structure

---
+++ src/App/Calendar.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace Calendar\App;
+
+use Calendar\Enum\ISO8601DayOfWeek;
+
+class Calendar
+{
+    /**
+     * @var Calendar
+     */
+    private static $staticInstance;
+
+    /**
+     * @var \DateTimeZone
+     */
+    private $timezone;
+
+    /**
+     * @var string|\DateTime
+     */
+    private $startDate;
+
+    /**
+     * @var string|\DateTime
+     */
+    private $endDate;
+
+    public static function today()
+    {
+        return new \DateTime('now', self::getTimezone());
+    }
+
+    public static function tomorrow()
+    {
+        $todayDate = self::today();
+        $todayDate->add(new \DateInterval('P1D'));
+
+        return $todayDate;
+    }
+
+    public static function yesterday()
+    {
+        $todayDate = self::today();
+        $todayDate->sub(new \DateInterval('P1D'));
+
+        return $todayDate;
+    }
+
+    /**
+     * @param string|\DateTime $startDate
+     * @param string|\DateTime $endDate
+     * @return Calendar
+     * @throws \Exception
+     */
+    public static function interval($startDate, $endDate = '')
+    {
+        $calendarInstance = self::getCalendarInstance();
+        if (!$startDate instanceof \DateTime) {
+            $startDate = new \DateTime($startDate, self::getTimezone());
+        }
+
+        if (empty($endDate)) {
+            $endDate = clone $startDate;
+        } elseif (!$endDate instanceof \DateTime) {
+            $endDate = new \DateTime($endDate, self::getTimezone());
+        }
+
+        // Trick to consider the informed last day, since \DatePeriod ignores it.
+        $endDate->setTime(0, 0, 1);
+
+        $calendarInstance->startDate = $startDate;
+        $calendarInstance->endDate = $endDate;
+
+        return $calendarInstance;
+    }
+
+    public function beforeDays($numberOfDays)
+    {
+        $this->endDate->sub(new \DateInterval('P' . $numberOfDays . 'D'));
+        $this->endDate->setTime(0, 0, 0);
+
+        return $this->endDate;
+    }
+
+    public function nextDays($numberOfDays)
+    {
+        $this->endDate->add(new \DateInterval('P' . $numberOfDays . 'D'));
+        $this->endDate->setTime(0, 0, 0);
+
+        return $this->endDate;
+    }
+
+    public function onlyMondays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::MONDAY);
+    }
+
+    public function onlyTuesdays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::TUESDAY);
+    }
+
+    public function onlyWednesdays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::WEDNESDAY);
+    }
+
+    public function onlyThursdays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::THURSDAY);
+    }
+
+    public function onlyFridays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::FRIDAY);
+    }
+
+    public function onlySaturdays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::SATURDAY);
+    }
+
+    public function onlySundays()
+    {
+        return $this->getOnlyCustomDayOfWeek(ISO8601DayOfWeek::SUNDAY);
+    }
+
+    public function onlyFirstDayOfMonths()
+    {
+        $firstDayOfMonths = [];
+        $datePeriod = new \DatePeriod($this->startDate, new \DateInterval('P1M'), $this->endDate);
+
+        foreach ($datePeriod as $dateTime) {
+            $firstDayOfMonths[] = new \DateTime($dateTime->format('Y-m-01'));
+        }
+
+        return $firstDayOfMonths;
+    }
+
+    public function onlyLastDayOfMonths()
+    {
+        $lastMonthDays = [];
+        $datePeriod = new \DatePeriod($this->startDate, new \DateInterval('P1M'), $this->endDate);
+        foreach ($datePeriod as $dateTime) {
+            $lastMonthDays[] = new \DateTime($dateTime->format('Y-m-t'));
+        }
+
+        return $lastMonthDays;
+    }
+
+    public function nextMonday()
+    {
+        return $this->getDesiredNextDayOfWeek('monday');
+    }
+
+    public function nextTuesday()
+    {
+        return $this->getDesiredNextDayOfWeek('tuesday');
+    }
+
+    public function nextWednesday()
+    {
+        return $this->getDesiredNextDayOfWeek('wednesday');
+    }
+
+    public function nextThursday()
+    {
+        return $this->getDesiredNextDayOfWeek('thursday');
+    }
+
+    public function nextFriday()
+    {
+        return $this->getDesiredNextDayOfWeek('friday');
+    }
+
+    public function nextSaturday()
+    {
+        return $this->getDesiredNextDayOfWeek('saturday');
+    }
+
+    public function nextSunday()
+    {
+        return $this->getDesiredNextDayOfWeek('sunday');
+    }
+
+    public static function getTimezone()
+    {
+        if (is_null(self::$staticInstance)) {
+            self::$staticInstance = new Calendar;
+        }
+
+        return (self::getCalendarInstance())->timezone ?: new \DateTimeZone;
+    }
+
+    public static function setTimezone($timezone)
+    {
+        if (is_null(self::$staticInstance)) {
+            self::$staticInstance = new Calendar;
+        }
+
+        self::$staticInstance->timezone = new \DateTimeZone($timezone);
+
+        return self::$staticInstance;
+    }
+
+    /**
+     * @param $targetNumDayOfWeek
+     * @return \DateTime[]
+     */
+    private function getOnlyCustomDayOfWeek($targetNumDayOfWeek)
+    {
+        $datePeriod = new \DatePeriod($this->startDate, new \DateInterval('P1D'), $this->endDate);
+        $targetDayOfWeek = [];
+
+        foreach ($datePeriod as $dateTime) {
+            $ISO_dayOfWeek = $dateTime->format('N');
+            if ((int) $ISO_dayOfWeek === $targetNumDayOfWeek) {
+                $targetDayOfWeek[] = $dateTime;
+            }
+        }
+
+        return $targetDayOfWeek;
+    }
+
+    private function getDesiredNextDayOfWeek($targetDay)
+    {
+        $allowedDaysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        if (in_array($targetDay, $allowedDaysOfWeek, true)) {
+            $this->endDate->modify('next ' . $targetDay);
+        }
+
+        return $this->endDate;
+    }
+
+    private static function getCalendarInstance()
+    {
+        if (is_null(self::$staticInstance)) {
+            self::$staticInstance = new Calendar;
+        }
+
+        return self::$staticInstance;
+    }
+}
---
+++ src/Enum/ISO8601DayOfWeek.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Calendar\Enum;
+
+class ISO8601DayOfWeek
+{
+    const MONDAY = 1;
+
+    const TUESDAY = 2;
+
+    const WEDNESDAY = 3;
+
+    const THURSDAY = 4;
+
+    const FRIDAY = 5;
+
+    const SATURDAY = 6;
+
+    const SUNDAY = 7;
+}
